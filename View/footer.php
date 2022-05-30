9ebbfec3b1f72a0a65fcc2ab23fc6bfe60ef03a6
Franck-Costanzo <franck.costanzo@laplateforme.io>
2022-05-30 10:04:56 +0200

    creation du footer

---
+++ View/footer.php
@@ -0,0 +1,27 @@
+<nav>
+    <h5>Liens Administratifs</h5>
+    <a href="">test</a>
+    <a href=""></a>
+    <a href=""></a>
+    <a href=""></a>
+    <a href=""></a>
+    <a href=""></a>
+    <a href=""></a>
+    <a href=""></a>
+</nav>
+
+<nav>
+    <h5>Education</h5>
+    <a href=""></a>
+    <a href=""></a>
+    <a href=""></a>
+</nav>
+
+<nav>
+    <h5>Liens du site</h5>
+    <a href=""></a>
+    <a href=""></a>
+    <a href=""></a>
+    <a href=""></a>
+    <a href=""></a>
+</nav>
