8c1394f3cb1dc79f6ceeeeb8934cb2a397c35ba1
Franck-Costanzo <franck.costanzo@laplateforme.io>
2022-06-29 10:07:30 +0200

    CSS - amélioration des img carousel et cards

--- View/JS/404.js
+++ View/JS/404.js
@@ -1,48 +1,41 @@
 export default function f404() {
 
-    //----setting variable for main div to append moving img
     let main404 = document.querySelector('main');
+    let main404Height = main404.offsetHeight;
+    let main404Width = main404.offsetWidth;
 
-        //getting it's height
-        let main404Height = main404.offsetHeight;
-
-    //----creating the image, styling it and addint it to the DOM
     let image404 = document.createElement('img');
     image404.src = './View/Media/logo-sel-homme-ballon-seul.svg';
     image404.style.position = 'absolute';
     image404.style.width = '4%';
-    image404.style.bottom = '0px';
+
     main404.appendChild(image404);
 
-    //----creating a KeyFrameEffect on the image that will translate it vertically
-    var imgKeyFrame = new KeyframeEffect(
-        image404,
-        [
-            { transform: 'translateY(0px)', left: parseInt(Math.round(Math.random() *( main404.offsetWidth - 0))) + 'px' },
-            { transform: 'translateY(-'+main404Height+'px)', left : parseInt(Math.round(Math.random() *( main404.offsetWidth - 0))) + 'px'}
-        ], 
+    let count = 0;
+
+    function randomPosition() {        
+        image404.style.bottom = '0px';
+        let leftPos = parseInt(Math.round(Math.random() *( main404.offsetWidth - 0)));
+        image404.style.left = leftPos + 'px'; 
+    }
+
+    randomPosition();
+    function increaseHeight()
+    {
+        if (image404.style.bottom != (main404Height+'px'))
         {
-            duration: parseInt(Math.round(Math.random() * 7500))           
+            count += 1;
+            image404.style.bottom = count+"px";
         }
-    );
+        else
+        {
+            count = 0;
+            image404.style.bottom = '0px';
+            randomPosition();
+        }
+    }
 
-    //----creating an animation using the keyFrameEffect
-    var imgAnim = new Animation(imgKeyFrame, document.timeline);
-        
-        //launching the animation
-        imgAnim.play();
 
-        //randomising again the starting point and its velocity
-        imgAnim.onfinish = () => (
-            imgKeyFrame.setKeyframes([
-                { transform: 'translateY(0px)', left: parseInt(Math.round(Math.random() *( main404.offsetWidth - 0))) + 'px' },
-                { transform: 'translateY(-'+main404Height+'px)', left : parseInt(Math.round(Math.random() *( main404.offsetWidth - 0))) + 'px'}
-                ]),
-            imgKeyFrame.updateTiming( 
-                {
-                    duration: parseInt(Math.round(Math.random() * 7500))   
-                }), 
-            imgAnim.play()
-        )    
+    setInterval(increaseHeight, 12);
     
 }
